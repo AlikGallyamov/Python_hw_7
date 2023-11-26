7d25871939b2d453cd6256741aa086f14922d018
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-11-26 23:29:39 +0500

    Написал метод, который упаковывает файлы zip архив

--- test_files.py
+++ test_files.py
@@ -1,6 +1,34 @@
-import os.path
+import os
+from zipfile import ZipFile
 
 CURRENT_PATH = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_PATH)
-LOAD_PATH = os
 
+
+# def generated_list():
+#     """
+#     Генерируем список с фильтрацией по формату файла
+#     """
+#     return [file_name for file_name in os.listdir(CURRENT_DIR) if
+#             file_name.endswith(".csv") or file_name.endswith(".pdf") or file_name.endswith(".xlsx")]
+#
+#
+# def file_add_in_archive(archive_name: str):
+#     """
+#     archive_name: имя создаваемого архива.
+#     Вызываем метод геренации списка имён текущей директории.
+#     Вернувшийся список упаковываем в zip архив
+#     """
+#     list_with_file_name = generated_list()
+#
+#     for file_name in list_with_file_name:
+#         with ZipFile(str(archive_name) + ".zip", "a") as zf:
+#             zf.write(file_name)
+#
+#
+# file_add_in_archive("archive")
+
+def read_content():
+    with ZipFile("archive.zip", "r") as zf:
+        content = zf.read("hello5.txt")
+        print(content)
