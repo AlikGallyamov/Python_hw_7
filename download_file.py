9719b27fc57086de1e62f1c2afa8979bbc8ccb1f
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-11-27 03:19:39 +0500

    Добавил загрузку файлов

---
+++ download_file.py
@@ -0,0 +1,40 @@
+from selene import browser, query
+from packing_files import source_dir
+import os
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+options = webdriver.ChromeOptions()
+prefs = {
+    "download.default_directory": source_dir,
+    "download.prompt_for_download": False
+}
+options.add_experimental_option("prefs", prefs)
+options.add_argument('--headless')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser.config.driver = driver
+
+
+def download_csv():
+    browser.open("https://github.com/AlikGallyamov/Python_hw_7/blob/main/files/exampleCSV.csv")
+    download_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
+    content = requests.get(url=download_url).content
+    with open(os.path.join(source_dir, "exampleCSV.csv"), 'wb') as file:
+        file.write(content)
+
+
+def download_pdf():
+    browser.open("https://github.com/AlikGallyamov/Python_hw_7/blob/main/files/examplePdf.pdf")
+    browser.element("[data-testid=download-raw-button]").click()
+    time.sleep(3)
+
+
+def download_xlsx():
+    browser.open("https://github.com/AlikGallyamov/Python_hw_7/blob/main/files/exampleXLSX.xlsx")
+    download_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
+    content = requests.get(url=download_url).content
+    with open(os.path.join(source_dir, "exampleXLSX.xlsx"), 'wb') as file:
+        file.write(content)
--- packing_files.py
+++ packing_files.py
@@ -1,6 +1,5 @@
 import zipfile, os
 
-
 CURRENT_PATH = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_PATH)
 
@@ -11,15 +10,10 @@ files_name = os.listdir(source_dir)
 
 def create_zip_files():
     """
-    archive_name: имя создаваемого архива.
-    Вызываем метод геренации списка имён текущей директории.
-    Вернувшийся список упаковываем в zip архив
+
     """
     with zipfile.ZipFile(resource_dir + '/test.zip', mode='w',
                          compression=zipfile.ZIP_DEFLATED) as zf:
         for file in files_name:
             add_file = os.path.join("files", file)
             zf.write(add_file)
-
-
-create_zip_files()
