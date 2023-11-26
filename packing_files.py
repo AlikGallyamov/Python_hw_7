19961f4e7c220ea45e26b995c54e592352ee803e
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-11-27 01:45:56 +0500

    Изменил реализацию, больше не генерируем список имён файлов, а используем метод listdir

---
+++ packing_files.py
@@ -0,0 +1,26 @@
+import zipfile, os
+import files
+import resources
+
+CURRENT_PATH = os.path.abspath(__file__)
+CURRENT_DIR = os.path.dirname(CURRENT_PATH)
+
+source_dir = os.path.join(CURRENT_DIR, "files")
+resource_dir = os.path.join(CURRENT_DIR, "resources")
+files_name = os.listdir(source_dir)
+
+
+def create_zip_files():
+    """
+    archive_name: имя создаваемого архива.
+    Вызываем метод геренации списка имён текущей директории.
+    Вернувшийся список упаковываем в zip архив
+    """
+    with zipfile.ZipFile(resource_dir + '/test.zip', mode='w',
+                         compression=zipfile.ZIP_DEFLATED) as zf:
+        for file in files_name:
+            add_file = os.path.join("files", file)
+            zf.write(add_file)
+
+
+create_zip_files()
