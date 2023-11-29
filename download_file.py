4ed662a7a717255770075bed26fce9b0575a8b19
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-11-29 20:45:30 +0500

    Убрал принты

--- download_file.py
+++ download_file.py
@@ -52,7 +52,5 @@ def create_zip_files():
     with zipfile.ZipFile(resource_dir + '/test.zip', mode='w',
                          compression=zipfile.ZIP_DEFLATED) as zf:
         for file in files_name:
-            print(file)
             add_file = os.path.join("files", file)
             zf.write(add_file)
-    print("Я заархивировался")
